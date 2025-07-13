f5493cec11d49696b29a081fd564c1d81a94f33c
“maribellamoreno” <maribellavmoreno@gmail.com>
2025-07-12 22:03:14 -0400

    Actualiznado proyecto con test basicos

--- test/api.personas.spec.js
+++ test/api.personas.spec.js
@@ -1,90 +1,80 @@
-const request = require('supertest')
-const { app, server } = require('../app')
+const request = require('supertest');
+const { app, server } = require('../app');
 
 const persona = {
-  nombre: 'Pepito',
-  apellido: 'Juan',
-  edad: 20,
+    nombre: 'Maria',
+    apellido: 'Lopez',
+    edad: 28
 }
-
 let personaId
+describe("Verificar modelo personas en API Propia", () => {
 
-// 1. Registrar una persona.
-describe('Validar creacion de una persona en el JSON', () => {
-  test('Deberia retornar codigo 400 con datos incompletos', async () => {
-    const response = await request(app).post('/personas').send({})
+    test("Validando si existen personas registradas", async () => {
+        const response = await request(app).get("/personas");
 
-    expect(response.statusCode).toBe(400)
-  })
+        //Arreglo no vacio.
+        expect(response.body).not.toHaveLength(0);
+        expect(response.statusCode).toBe(200);
 
-  test('Deberia crear una persona', async () => {
-    const response = await request(app).post('/personas').send(persona)
+    });
 
-    expect(response.statusCode).toBe(201)
-    expect(response.body).toHaveProperty('id')
-    personaId = response.body.id
-    expect(response.body.nombre).toBe(persona.nombre)
-  })
-})
-
-// 2. Consultar todas las personas.
-describe('Verificar modelo personas en API Propia', () => {
-  test('Deberia retornar todas las personas', async () => {
-    const response = await request(app).get('/personas')
-
-    expect(response.body).toBeInstanceOf(Array)
-    expect(response.body).not.toHaveLength(0)
-    expect(response.statusCode).toBe(200)
-  })
-})
-
-// 3. Consultar la persona que inserté
-describe('Validar la persona que se ingresa', () => {
-  test('Deberia retornar la persona creada anteriormente', async () => {
-    const response = await request(app).get(`/personas/${personaId}`)
-
-    expect(response.body.id).toBe(personaId)
-    expect(response.body).toBeInstanceOf(Object)
-  })
-})
-
-// 4. Actualizar la persona.
-describe('Validar la actualizacion de una persona', () => {
-  test('Deberia retornar la persona actualizada', async () => {
-    const response = await request(app).put(`/personas/${personaId}`).send({
-      nombre: 'Vicente',
+    //Registrar una persona
+describe('Validar creación persona', () => {
+    test('Inserción de persona', async () => {
+        const response = await request(app).post('/personas').send(persona) //Inserción persona con post
+        expect(response.statusCode).toBe(201) //Validar código 201
+        expect(response.body).toHaveProperty('id') //Validar que retorne id
+        personaId = response.body.id
+        expect(response.body.nombre).toBe(persona.nombre) //Validar que el nombre enviado sea el mismo retornado
     })
-
-    expect(response.body.nombre).toBe('Vicente')
-  })
 })
-
-// 5. Borrar la persona.
-describe('Validar el borrado de la persona en el registro', () => {
-  test('Deberia retornar codigo 404 con usuario no registrado', async () => {
-    const response = await request(app).delete(/personas/65465459665)
-
-    expect(response.status).toBe(404)
-  }) /*  */
-
-  test('Deberia retornar un status 204', async () => {
-    const response = await request(app).delete(`/personas/${personaId}`)
-
-    expect(response.status).toBe(204)
-  })
+    //Consultar todas las personas
+describe('Validar consulta de personas', () => {
+    test('Consulta de todas las personas', async () => {
+        const response = await request(app).get('/personas') //Consulta de data con get
+        expect(response.body).toBeInstanceOf(Array) //Validar que retorne un array
+        expect(response.body).not.toHaveLength(0) //Validar que el arreglo no esté vacío
+        expect(response.statusCode).toBe(200) //Validar retorno código 200
+    })
 })
-
-// 6. Consultar todas las personas.
-describe('Verificar modelo personas en API Propia', () => {
-  test('Deberia retornar un array vacio', async () => {
-    const response = await request(app).get('/personas')
-
-    expect(response.body).toHaveLength(0)
-  })
-
-  afterAll((done) => {
-    // 3. Al final de todo, cerramos el servidor para que Jest no se quede colgado.
-    console.log('--- [afterAll] Cerrando servidor ---')
-    server.close(done)
-  })
+    //Consultar la persona que inserté
+describe('Validar consulta de personas insertadas', () => {
+    test('Consulta de persona insertada', async () => {
+        const response = await request(app).get(`/personas/${personaId}`) //Consulta de persona insertada
+        expect(response.body.id).toBe(personaId) //Validar id de persona
+        expect(response.body).toBeInstanceOf(Object)//Validar retorno un objeto
+    })
 })
+    //Actualizar la persona
+describe('Validar actualizacion de personas', () => {
+    test('Consulta de persona actualizada', async () => {
+        const response = await request(app).put(`/personas/${personaId}`).send({
+            nombre: 'Mariana', //Actualización nombre
+            apellido: 'Vega' //Actualización apellido
+        })
+        expect(response.body.nombre).toBe('Mariana') //Validar actualización nombre de persona
+        expect(response.body.apellido).toBe('Vega') //Validar actualización apellido de persona
+    })
+})
+    //Borrar la persona
+describe('Validar borrado de persona insertada', () => {
+    test('Debe borrar la persona y retornar codigo 204', async () => {
+        const response = await request(app).delete(`/personas/${personaId}`)
+        expect(response.statusCode).toBe(204) //Validar que el código de retorno sea 204
+    })
+})
+    //Consultar todas las personas
+describe('Validar arreglo vacío al consultar personas', () => {
+    test('Debe retornar un arreglo vacío', async () => {
+        const response = await request(app).get('/personas/')
+        expect(response.statusCode).toBe(200) //Validar que responde correctamente
+        expect(Array.isArray(response.body)).toBe(true) //Validar que es un arreglo
+        expect(response.body).toHaveLength(0) //Validar que el arreglo está vacío
+    })
+})
+    afterAll((done) => {
+        // 3. Al final de todo, cerramos el servidor para que Jest no se quede colgado.
+        console.log("--- [afterAll] Cerrando servidor ---");
+        server.close(done);
+    });
+});
--- test/dummy.json.spec.js
+++ test/dummy.json.spec.js
@@ -1,30 +1,30 @@
 const request = require('supertest');
 require('dotenv').config();
 
- const product = {
-            title: 'Iphone 18',
-            price:500
-        };
+
+const product = {
+    title: 'Iphone 18',
+    price: 500
+};
 
 describe("Verificar Modelo Products de DummyJSON", () => {
 
     test("Validar código de retorno de consulta de productos", async () => {
-        const response = await request(process.env.BASE_URL).get("/products"); 
+        const response = await request(process.env.BASE_URL).get("/products");
         expect(response.statusCode).toBe(200);
         expect(response.body).toBeInstanceOf(Object);
         expect(response.body.products).toBeInstanceOf(Array);
     });
 
-    test("Validar que nombre del producto corresponda", async () => {
+    test("Validar que el nombre del producto corresponda", async () => {
         const nombreEsperado = "Essence Mascara Lash Princess";
-        
-        const response = await request(process.env.BASE_URL).get(`/products/${Number(process.env.ID_PRODUCTO_PRUEBA)}`);
+        const response = await request(process.env.BASE_URL).get(`/products/${process.env.ID_PRODUCTO_PRUEBA}`);
         expect(response.body.title).toBe(nombreEsperado);
     });
 
-    //Prueva de inserción con POST
     test("Validar inserción de producto", async () => {
-       
+
+
         const response = await request(process.env.BASE_URL).post(`/products/add`).send(product);
 
         expect(response.statusCode).toBe(201);
@@ -33,25 +33,23 @@ describe("Verificar Modelo Products de DummyJSON", () => {
         expect(response.body.price).toBe(product.price);
     });
 
-    //Prueva actualizacion de producto con PUT
-    test("Actualizacion de producto", async () => {
-        const response = await request(process.env.BASE_URL).patch(`/products/${Number(process.env.ID_PRODUCTO_PRUEBA)}`).send(product);
+    test("Actualización de producto", async () => {
+        const response = await request(process.env.BASE_URL).patch(`/products/${process.env.ID_PRODUCTO_PRUEBA}`).send(product);
 
         expect(response.statusCode).toBe(200);
         expect(response.body).toBeInstanceOf(Object);
         expect(response.body).toHaveProperty("id");
         expect(response.body.id).toEqual(Number(process.env.ID_PRODUCTO_PRUEBA));
+
     });
 
-    //Prueva borrar producto con DELETE
     test("Borrado de producto", async () => {
-        const response = await request(process.env.BASE_URL).delete(`/products/${Number(process.env.ID_PRODUCTO_PRUEBA)}`);
+        const response = await request(process.env.BASE_URL).delete(`/products/${process.env.ID_PRODUCTO_PRUEBA}`);
 
         expect(response.statusCode).toBe(200);
         expect(response.body).toBeInstanceOf(Object);
         expect(response.body).toHaveProperty("isDeleted", true);
         expect(response.body.id).toEqual(Number(process.env.ID_PRODUCTO_PRUEBA));
     });
-
 
 });
